a843817840e9f1abefc2b6aa7b24eb26924beeb3
dbmil <143900261+dbmil@users.noreply.github.com>
2023-12-12 12:58:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var titles = ["COMMONWEALTH OF NATIONS", "NEW WEBSITE"];
-var dates = ["9/2/23", "9/2/23"];
-var articles = ["The Dingabadonga Military starts the new school year off with new alliances. The Commonwealth of Nations, owned by Adam Olszewski, is the new country game for this school year.", "The Dingabadonga Military launches their new website. This new website has a better design and includes all of the same features from the original website. Site security and user accessibility has also been improved."];
+var titles = ["NEWEST COUNTRY GAME", "COMMONWEALTH OF NATIONS", "NEW WEBSITE"];
+var dates = ["12/12/23", "9/2/23", "9/2/23"];
+var articles = ["The newest country game of 2023 it looking for more people to sign up! If you are interested in participating, fill out the form link in this article.<br>https://forms.gle/mNCVJAnmwjcGRiXk7", "The Dingabadonga Military starts the new school year off with new alliances. The Commonwealth of Nations, owned by Adam Olszewski, is the new country game for this school year.", "The Dingabadonga Military launches their new website. This new website has a better design and includes all of the same features from the original website. Site security and user accessibility has also been improved."];
 var article = 0;
 function right() {
 article += 1;
